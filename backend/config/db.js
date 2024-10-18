707703d83f646fad7fda7f3adf16d56d07e00763
Ruthless2409 <vaib2409@gmail.com>
2024-10-18 16:50:52 +0530

    connected to mongodb

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,11 +1,13 @@
 import mongoose from "mongoose";
-const connect = async () => {
+const connectDB = async () => {
     try{
-        await mongoose.connect(process.env.MONGO_URI)
-        console.log('Succesfully connected to MongoDB')
+        await mongoose.connect(process.env.MONGO_URI);
+        console.log('Succesfully connected to MongoDB');
 
     } catch(error){
         console.error(`Error: ${error.message}`);
-        process.exit(1)
+        process.exit(1);
     }
 }
+
+export default connectDB;
--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,26 @@
-console.log("yea ");
+import express from "express";
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import path from "path";
+
+//files
+import connectDB from "./config/db.js";
+
+//Config
+dotenv.config();
+connectDB();
+
+const app = express();
+
+//middlewares
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
+
+const PORT = process.env.PORT || 3000;
+
+//Routes
+
+
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
