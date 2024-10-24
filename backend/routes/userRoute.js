655e5e906e999074514053174b9140aab2b017ed
Ruthless2409 <vaib2409@gmail.com>
2024-10-24 18:36:53 +0530

    get current user profile

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,7 +2,8 @@ import express from "express";
 
 //controllers
 import { createUser, loginUser 
-    ,logoutCurrentUser, getAllUsers
+    ,logoutCurrentUser, getAllUsers,
+    getCurrentUserProfile
 } from "../controllers/userController.js";
 //middlewares
 import { authenticate, authoriseAdmin } from "../middlewares/authMiddleware.js";
@@ -12,5 +13,6 @@ const router = express.Router();
 router.route("/").post(createUser).get(authenticate,authoriseAdmin, getAllUsers);
 router.route("/auth").post(loginUser);
 router.route("/logout").post(logoutCurrentUser);
+router.route('/profile').get(authenticate, getCurrentUserProfile);
 
 export default router;
